e9c2f0fea80c9f8370f95d447f10dadff96a0acc
Swift-Nite <74127135+swiftnite@users.noreply.github.com>
2021-03-20 11:52:39 +1100

    Update shop.py

--- shop.py
+++ shop.py
@@ -14,43 +14,47 @@ point = customisation.point
 print("\n\nWelcome to Swift-Nite's Shop Section Bot!\n\n")
 
 def main():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    response = get('https://fn-api.com/api/shop_categories')
-    if response:
-        main = get('https://fn-api.com/api/shop_categories')
-        stamp = main.json()['timestamp']
-        res = main.json()['shopCategories']
-        with open('cache.json', 'r') as fnapi_file:
-            old = json.load(fnapi_file)
-        if stamp != old:
-            print("changes detected!")
-            my_file = open(f"sections.txt","w+")
-            my_file.write(f"{Heading}\n\n")
-            for i in res:
-                name=i["sectionName"]
-                if i["quantity"] != 1:
-                    quantity=i["quantity"]
-                    if Brackets == "True":
-                        quantity=f" (X{quantity})"
-                    elif Brackets == "False":
-                        quantity=f" x{quantity}"
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        response = get('https://fn-api.com/api/shop_categories')
+        if response:
+            main = get('https://fn-api.com/api/shop_categories')
+            stamp = main.json()['timestamp']
+            res = main.json()['shopCategories']
+            with open('cache.json', 'r') as fnapi_file:
+                old = json.load(fnapi_file)
+            if stamp != old:
+                print("changes detected!")
+                my_file = open(f"sections.txt","w+")
+                my_file.write(f"{Heading}\n\n")
+                for i in res:
+                    name=i["sectionName"]
+                    if i["quantity"] != 1:
+                        quantity=i["quantity"]
+                        if Brackets == "True":
+                            quantity=f" (X{quantity})"
+                        elif Brackets == "False":
+                            quantity=f" X{quantity}"
+                        else:
+                            quantity=f" (X{quantity})"
                     else:
-                        quantity=f" (X{quantity})"
-                else:
-                    quantity=''
-                my_file.write(f"{point}{name}{quantity}\n")
-            my_file = open(f"sections.txt","r")
-            file_contents = my_file.read()
-            print (file_contents)
-            api.update_status(f"{file_contents}")
-            print("Posted!")
-            my_file.close()
-            with open('cache.json', 'w') as file:
-                json.dump(stamp, file, indent=3)
-    else:
+                        quantity=''
+                    my_file.write(f"{point}{name}{quantity}\n")
+                my_file = open(f"sections.txt","r")
+                file_contents = my_file.read()
+                print (file_contents)
+                api.update_status(f"{file_contents}")
+                print("Posted!")
+                my_file.close()
+                with open('cache.json', 'w') as file:
+                    json.dump(stamp, file, indent=3)
+        else:
+            print("error")
+    except:
         print("error")
+            
 if __name__ == "__main__":
     while True:
         print("Checking for section changes!")
